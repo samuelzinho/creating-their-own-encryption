c49f47d27891a59b8cc656f4efd16dcc418db62a
KoubackTr <koubacktr@gmail.com>
2014-07-07 20:37:12 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1.0"> 
+	
+	<title>Intranet</title>
+	<center>
+	<font size="8" color="#1E90FF">
+		<p>Sistema de login WEB com criptografia independente</p>
+	</font>
+		<a href="cadastro.html"><font size="5" color="#1E90FF">Cadastre-se</font></a> OU 
+		<a href="login.html"><font size="5" color="#1E90FF">Entre</font></a>
+	</font>
+	</center>
+	
+		<font size="4" color="#5E9EDC">
+		<br>About/Documents: <a href="docs/">/docs</a><br>
+		<p>By KoubackTr <br>
+			koubacktr@gmail.com <br>
+			http://koubacktr.wordpress.com/ <br>
+			</p>
+	</font>
+	
+<html>
